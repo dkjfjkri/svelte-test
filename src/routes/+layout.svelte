2f47d4baa3910a2bee32413c17a064fb716ac1fd
Matvey <SHevtsovMD@chgtk.ru>
2023-05-24 18:48:39 +0800

    add all

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<link rel="icon" href="./favicon.png" />
+		<meta name="viewport" content="width=device-width" />
+		<style>
+			body {
+			  background-position: left top;
+			  font-family: "Inter", sans-serif;
+			  margin: 0;
+			  background-repeat: no-repeat;
+			}
+	  
+			* {
+			  box-sizing: border-box;
+			}
+	  
+			a {
+			  text-decoration: none;
+			  color: black;
+			}
+	  
+			nav {
+			  position: sticky;
+			  top: 0;
+			  left: 0;
+			  right: 0;
+			  margin: 0 80px 0 80px;
+			  padding-top: 2rem;
+			  padding-bottom: 2rem;
+			}
+	  
+			nav .nav-container {
+			  display: flex;
+			  justify-content: space-between;
+			  align-items: center;
+			}
+	  
+			.nav-links {
+			  display: flex;
+			  justify-content: flex-end;
+			  align-items: center;
+			  margin: -1em 0;
+			  flex: 1 0 auto;
+			}
+	  
+			.nav-links .link {
+			  position: relative;
+			  margin: 1em;
+			  font-size: 0.85rem;
+			  letter-spacing: 1px;
+			  color: black;
+			}
+	  
+			.nav-logo {
+			  position: relative;
+			  line-height: 1;
+			  font-size: 1.1rem;
+			  color: black;
+			}
+		  </style>
+		
+		<link href="./_app/immutable/assets/_layout.65099de4.css" rel="stylesheet">
+		<link href="./_app/immutable/assets/_page.abab7d99.css" rel="stylesheet"><title>Имя Фамилия</title><!-- HEAD_svelte-vshy5s_START --><!-- HEAD_svelte-vshy5s_END -->
+	</head>
+	<body data-sveltekit-preload-data="hover">
+		<div style="display: contents">
+
+
+<nav><div class="nav-container"><a href="/" class="nav-logo" title="Back to Homepage">Матвей Шевцов</a>
+        <div class="nav-links"><a href="/about" class="link">Обо мне</a><a href="/blog" class="link">Блог</a><a href="/project" class="link">Портфолио</a><a href="/contact" class="link">Контакты</a></div></div></nav>
+
+<div class="container svelte-1m8mvlz">
+    
+
+<div class="container svelte-etkpxf"><div class="info svelte-etkpxf"><div class="animate-background"><div class="container info-container svelte-etkpxf"><div class="info-text svelte-etkpxf"><h1 class="svelte-etkpxf"><p class="animate-text-1 svelte-etkpxf">Персональный сайт,</p>
+                    <p class="text-2 svelte-etkpxf">Матвей Шевцов</p></h1></div>
+                <h2 class="subtitle svelte-etkpxf">
+                </h2>
+                <div style="color:black" class="scroll-button animate-scroll svelte-etkpxf" tabindex="0"><a href="/about">Узнать обо мне</a><div class="arrow svelte-etkpxf">→</div></div></div></div></div></div>
+
+<div class="background-div svelte-etkpxf"></div>
+</div>
+
+
+			
+			<script>
+				{
+					__sveltekit_to3qk8 = {
+						base: new URL(".", location).pathname.slice(0, -1),
+						env: {}
+					};
+
+					const element = document.currentScript.parentElement;
+
+					const data = [null,null];
+
+					Promise.all([
+						import("./_app/immutable/entry/start.1ee0651f.js"),
+						import("./_app/immutable/entry/app.73eb89e8.js")
+					]).then(([kit, app]) => {
+						kit.start(app, element, {
+							node_ids: [0, 7],
+							data,
+							form: null,
+							error: null
+						});
+					});
+				}
+			</script>
+		</div>
+	</body>
+</html>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,83 @@
+<svelte:head>
+    <title>........</title>
+</svelte:head>
+
+<div class="container">
+    <div class="info">
+        <div class="animate-background">
+            <div class="container info-container">
+                <div class="info-text">
+                    <h1>
+                        <p class="animate-text-1">.......</p>
+                        <p class="text-2">.....</p>
+                    </h1>
+                </div>
+                <h2 class="subtitle">
+                    <p>...... </p>
+                </h2>
+                <div style="color:black" class="scroll-button animate-scroll" tabindex="0">
+                    <a href="/about">Начнём...</a>
+                    <div class="arrow">→</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="background-div" />
+
+<style>
+    .info {
+        text-align: center;
+        position: relative;
+    }
+    .info-container {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: left;
+        align-items: start;
+    }
+    .container {
+        max-width: 1400px;
+        margin: 190px 0 0 2.5rem;
+    }
+    .info h1 {
+        font-size: 4rem;
+        margin: 0;
+    }
+    .info h1 p {
+        position: relative;
+        font: inherit;
+        margin: 0;
+        text-align: left;
+        line-height: 130%;
+        width: fit-content;
+        font-weight: 800;
+        letter-spacing: -0.03em;
+    }
+    .info h2 {
+        max-width: 65ch;
+        font-weight: 200;
+        margin-top: 0.5rem;
+    }
+    .background-div {
+        background-color: #f8d196;
+        width: 100%;
+        height: 56vh;
+        margin: -295px auto;
+    }
+    .info .info-text {
+        position: relative;
+    }
+    .scroll-button {
+        display: flex;
+        border: none;
+        outline: none;
+        cursor: pointer;
+    }
+    .scroll-button .arrow {
+        margin-left: 0.5rem;
+    }
+</style>
